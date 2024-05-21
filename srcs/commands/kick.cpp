fa0c7e8a6429d838ed896a530874e6389634ee02
Abdellatif Elfany <abelfany@e1r5p8.1337.ma>
2024-05-21 17:27:13 +0100

    i fix some i forget it in last commit ~_~

--- srcs/commands/kick.cpp
+++ srcs/commands/kick.cpp
@@ -6,7 +6,7 @@
 /*   By: abelfany <abelfany@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 02:24:03 by abelfany          #+#    #+#             */
-/*   Updated: 2024/05/21 16:03:53 by abelfany         ###   ########.fr       */
+/*   Updated: 2024/05/21 17:16:31 by abelfany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,13 @@
 
 void Channel::deletClient(std::string nick) {
     map_users::iterator it = _users.begin();
-    
     std::cout << "\033[0;31m { " << _users.size() << " }\033[0m" << std::endl;
     while (it != _users.end()) {
         if (it->first.second == nick) {
-            std::cout << "\033[0;31m { " << it->first.second << " }\033[0m" << std::endl;
             _users.erase(it++);
-        } else {
-            ++it;
         }
+        else
+            ++it;
     }
     std::cout << "\033[0;31m { " << _users.size() << " }\033[0m" << std::endl;
 }
@@ -33,6 +31,7 @@ void Servrr::split(std::string buffer, size_t i) {
     std::string user;
     std::string nick = getClientitoByIndex(i-1).getNickName();
     while (std::getline(users,user, ',')) {
+    std::cout << "\033[0;31m { " << user << " }\033[0m" << std::endl;
     try {
         Channel &mode = getChannel(args[1]);
         if(mode.checkUserexist(nick) == false)
@@ -43,8 +42,8 @@ void Servrr::split(std::string buffer, size_t i) {
             sendMsgToClient(getClientitoByIndex(i-1).getClinetFd(), ERR_WASNOSUCHNICK(host,args[1]));
         else {
             std::cout << "\033[0;31m { " << user << " }\033[0m" << std::endl;
-            SendToAll(mode, RPL_KICK(nick,getClientitoByIndex(i-1).getUserName(),host,args[2],args[1], "fuck"));
-            mode.deletClient(args[2]);
+            SendToAll(mode, RPL_KICK(nick,getClientitoByIndex(i-1).getUserName(),host,user,args[1], "fuck"));
+            mode.deletClient(user);
         }
     }
     catch(...) {
