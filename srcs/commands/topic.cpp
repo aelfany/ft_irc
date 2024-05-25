0e3a0fe8ad28e45f2531b62ba99f1c722312d286
Abdellatif Elfany <abelfany@e1r5p8.1337.ma>
2024-05-25 18:43:06 +0100

    i fix topic time from my foramt to unix timestamp format

--- srcs/commands/topic.cpp
+++ srcs/commands/topic.cpp
@@ -6,7 +6,7 @@
 /*   By: abelfany <abelfany@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:10:19 by abelfany          #+#    #+#             */
-/*   Updated: 2024/05/25 17:47:16 by abelfany         ###   ########.fr       */
+/*   Updated: 2024/05/25 18:39:47 by abelfany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,12 @@
 #include "../../include/Server.hpp"
 #include "../../include/client.hpp"
 
-//Sun Apr 21 17:43:10 2024
-// TTT set the topic at: Apr 21, 2024 at 6:12 PM
-
 std::string  Servrr::_time(std::string nick) {
+    (void)nick;
     time_t now = time(0);
-    std::string tmp = ctime(&now);
-    std::string time;
-    std::stringstream s(tmp);
-    std::string arr[5];
-    for(int a = 0; s >> arr[a]; a++) {}
-    time += nick + arr[1] + " " + arr[2] + ", " + arr[4] + " at " + arr[3];
-    std::cout << time << std::endl;
-    return time;
+    char buffer[20];
+    std::strftime(buffer, sizeof(buffer), "%s", std::localtime(&now));
+    return std::string(buffer);
 }
 void    Channel::setTopicAttr(std::string topic, bool topc, std::string seter, std::string time) {
     topicseter = seter;
@@ -37,6 +30,7 @@ void    Channel::setTopicAttr(std::string topic, bool topc, std::string seter, s
 void Servrr::Topic(std::string nick, size_t i) {
     try {
         Channel &mode = getChannel(args[1]);
+        mode.setTopicAttr(args[2], true, nick, _time(nick));
         // if(mode.checkUserexist(nick) == false) {
         //         sendMsgToClient(getClientitoByIndex(i-1).getClinetFd(), ERR_NOTONCHANNEL(host,nick));
         // }
@@ -48,7 +42,6 @@ void Servrr::Topic(std::string nick, size_t i) {
                 if (mode.getPrvBynickname(nick) == false && mode.getTopc() == true && mode.checkUserexist(nick) == true) {
                     sendMsgToClient(getClientitoByIndex(i-1).getClinetFd(), ERR_CHANOPRIVSNEEDED(host,nick));      
                 }
-                mode.setTopicAttr(args[2], true, nick, _time(nick));
                 SendToAll(mode, RPL_NEWTOPICSETTED(nick, mode.gettopicseter(), host,mode.getChannelName(),args[2]));
             }
         }
